a29b4664a56aa83e6b82206cf295e27eeaa2d0ac
Agung Dirgantara <agungmasda29@gmail.com>
2023-08-28 19:15:06 +0700

    chore: Customize for necessary configuration

---
+++ jest.config.ts
@@ -0,0 +1,24 @@
+import dotenv from 'dotenv';
+import { Config } from '@jest/types';
+
+// Environment for testing
+dotenv.config({ path: '.env.test' });
+
+const config: Config.InitialOptions = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/$1',
+  },
+  coveragePathIgnorePatterns: ['<rootDir>/main.ts'],
+  verbose: true,
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+};
+
+export default config;
