ec21055a11f7fc909dccdfaa423a8279da982911
Agung Dirgantara <agungmasda29@gmail.com>
2023-09-04 03:49:16 +0700

    test: ACL guard

--- src/common/guards/acl/acl.guard.spec.ts
+++ src/common/guards/acl/acl.guard.spec.ts
@@ -9,7 +9,7 @@ import { Permissions, Roles } from '@/common/decorators/acl/acl.decorator';
 // * Mock class
 @Roles(['ClassRole'])
 @Permissions(['ClassPermission'])
-class MockClass {
+class MockClass1 {
   // Test 1 : Activated by handler permission
   @Permissions(['HandlerPermission'])
   test1(): string {
@@ -33,6 +33,31 @@ class MockClass {
   }
 }
 
+// * Mock class
+class MockClass2 {
+  // Test 1 : Activated by handler permission
+  test1(): string {
+    return 'OK';
+  }
+
+  // Test 2 : Activated by handler role
+  test2(): string {
+    return 'OK';
+  }
+
+  // Test 3 : Activated by class permission
+  @Permissions(['HandlerPermission'])
+  test3(): string {
+    return 'OK';
+  }
+
+  // Test 4 : Activated by class role
+  @Roles(['HandlerRole'])
+  test4(): string {
+    return 'OK';
+  }
+}
+
 describe('ACL Guard', () => {
   const guard = new ACLGuard(new Logger(), new Reflector());
 
@@ -48,90 +73,145 @@ describe('ACL Guard', () => {
     let mockHttpArgumentsHost: Partial<HttpArgumentsHost>;
     let mockHttpExecutionContext: Partial<ExecutionContext>;
 
-    beforeEach(() => {
-      mockHttpRequest = createMock<Request>();
-      mockHttpArgumentsHost = {
-        getRequest: jest.fn().mockReturnValue(mockHttpRequest),
-        getNext: jest.fn(),
-      };
-      mockHttpExecutionContext = createMock<ExecutionContext>({
-        getType: jest.fn().mockReturnValue('http'),
-        switchToHttp: jest.fn().mockReturnValue(mockHttpArgumentsHost),
-        getClass: () => MockClass as any,
+    describe('Mock Class 1', () => {
+      beforeEach(() => {
+        mockHttpRequest = createMock<Request>();
+        mockHttpArgumentsHost = {
+          getRequest: jest.fn().mockReturnValue(mockHttpRequest),
+          getNext: jest.fn(),
+        };
+        mockHttpExecutionContext = createMock<ExecutionContext>({
+          getType: jest.fn().mockReturnValue('http'),
+          switchToHttp: jest.fn().mockReturnValue(mockHttpArgumentsHost),
+          getClass: () => MockClass1 as any,
+        });
+      });
+
+      describe('should return true', () => {
+        it('should MockClass1.test1 activated by handler permission', () => {
+          SetMetadata('Permissions', ['HandlerPermission'])(
+            mockHttpRequest.app,
+          );
+          const executionContext = createMock<ExecutionContext>({
+            ...mockHttpExecutionContext,
+            getHandler: () => new MockClass1().test1 as any,
+          });
+          expect(guard.canActivate(executionContext)).toBe(true);
+        });
+
+        it('should MockClass1.test2 activated by handler role', () => {
+          SetMetadata('Roles', ['HandlerRole'])(mockHttpRequest.app);
+          const executionContext = createMock<ExecutionContext>({
+            ...mockHttpExecutionContext,
+            getHandler: () => new MockClass1().test2 as any,
+          });
+          expect(guard.canActivate(executionContext)).toBe(true);
+        });
+
+        it('should MockClass1.test3 activated by class permission', () => {
+          SetMetadata('Permissions', ['ClassPermission'])(mockHttpRequest.app);
+          const executionContext = createMock<ExecutionContext>({
+            ...mockHttpExecutionContext,
+            getHandler: () => new MockClass1().test3 as any,
+          });
+          expect(guard.canActivate(executionContext)).toBe(true);
+        });
+
+        it('should MockClass1.test4 activated by class role', () => {
+          SetMetadata('Roles', ['ClassRole'])(mockHttpRequest.app);
+          const executionContext = createMock<ExecutionContext>({
+            ...mockHttpExecutionContext,
+            getHandler: () => new MockClass1().test4 as any,
+          });
+          expect(guard.canActivate(executionContext)).toBe(true);
+        });
+      });
+
+      describe('should return false', () => {
+        it('should return false when MockClass1.test1 requires role from handler', () => {
+          SetMetadata('Roles', ['HandlerRole'])(mockHttpRequest.app);
+          const executionContext = createMock<ExecutionContext>({
+            ...mockHttpExecutionContext,
+            getHandler: () => new MockClass1().test1 as any,
+          });
+          expect(guard.canActivate(executionContext)).toBe(false);
+        });
+
+        it('should return false when MockClass1.test2 requires permission from handler', () => {
+          SetMetadata('Permission', ['HandlerPermission'])(mockHttpRequest.app);
+          const executionContext = createMock<ExecutionContext>({
+            ...mockHttpExecutionContext,
+            getHandler: () => new MockClass1().test2 as any,
+          });
+          expect(guard.canActivate(executionContext)).toBe(false);
+        });
+
+        it('should return false when MockClass1.test3 requires permission from class', () => {
+          const executionContext = createMock<ExecutionContext>({
+            ...mockHttpExecutionContext,
+            getHandler: () => new MockClass1().test3 as any,
+          });
+          expect(guard.canActivate(executionContext)).toBe(false);
+        });
+
+        it('should return false when MockClass1.test4 requires role from class', () => {
+          const executionContext = createMock<ExecutionContext>({
+            ...mockHttpExecutionContext,
+            getHandler: () => new MockClass1().test4 as any,
+          });
+          expect(guard.canActivate(executionContext)).toBe(false);
+        });
       });
     });
 
-    describe('should return true', () => {
-      it('should test1 activated by handler permission', () => {
-        SetMetadata('Permissions', ['HandlerPermission'])(mockHttpRequest.app);
-        const executionContext = createMock<ExecutionContext>({
-          ...mockHttpExecutionContext,
-          getHandler: () => new MockClass().test1 as any,
+    describe('Mock Class 2', () => {
+      beforeEach(() => {
+        mockHttpRequest = createMock<Request>();
+        mockHttpArgumentsHost = {
+          getRequest: jest.fn().mockReturnValue(mockHttpRequest),
+          getNext: jest.fn(),
+        };
+        mockHttpExecutionContext = createMock<ExecutionContext>({
+          getType: jest.fn().mockReturnValue('http'),
+          switchToHttp: jest.fn().mockReturnValue(mockHttpArgumentsHost),
+          getClass: () => MockClass2 as any,
         });
-        expect(guard.canActivate(executionContext)).toBe(true);
       });
 
-      it('should test2 activated by handler role', () => {
-        SetMetadata('Roles', ['HandlerRole'])(mockHttpRequest.app);
-        const executionContext = createMock<ExecutionContext>({
-          ...mockHttpExecutionContext,
-          getHandler: () => new MockClass().test2 as any,
+      describe('should return true', () => {
+        it('should MockClass2.test1 activated by no rule', () => {
+          const executionContext = createMock<ExecutionContext>({
+            ...mockHttpExecutionContext,
+            getHandler: () => new MockClass2().test1 as any,
+          });
+          expect(guard.canActivate(executionContext)).toBe(true);
         });
-        expect(guard.canActivate(executionContext)).toBe(true);
+
+        it('should MockClass2.test2 activated by no rule', () => {
+          const executionContext = createMock<ExecutionContext>({
+            ...mockHttpExecutionContext,
+            getHandler: () => new MockClass2().test2 as any,
+          });
+          expect(guard.canActivate(executionContext)).toBe(true);
+        });
       });
 
-      it('should test3 activated by class permission', () => {
-        SetMetadata('Permissions', ['ClassPermission'])(mockHttpRequest.app);
-        const executionContext = createMock<ExecutionContext>({
-          ...mockHttpExecutionContext,
-          getHandler: () => new MockClass().test3 as any,
+      describe('should return false', () => {
+        it('should MockClass2.test3 rejected by handler permission', () => {
+          const executionContext = createMock<ExecutionContext>({
+            ...mockHttpExecutionContext,
+            getHandler: () => new MockClass2().test3 as any,
+          });
+          expect(guard.canActivate(executionContext)).toBe(false);
         });
-        expect(guard.canActivate(executionContext)).toBe(true);
-      });
 
-      it('should test4 activated by class role', () => {
-        SetMetadata('Roles', ['ClassRole'])(mockHttpRequest.app);
-        const executionContext = createMock<ExecutionContext>({
-          ...mockHttpExecutionContext,
-          getHandler: () => new MockClass().test4 as any,
+        it('should MockClass2.test4 rejected by handler role', () => {
+          const executionContext = createMock<ExecutionContext>({
+            ...mockHttpExecutionContext,
+            getHandler: () => new MockClass2().test4 as any,
+          });
+          expect(guard.canActivate(executionContext)).toBe(false);
         });
-        expect(guard.canActivate(executionContext)).toBe(true);
-      });
-    });
-
-    describe('should return false', () => {
-      it('should return false when test1 requires role from handler', () => {
-        SetMetadata('Roles', ['HandlerRole'])(mockHttpRequest.app);
-        const executionContext = createMock<ExecutionContext>({
-          ...mockHttpExecutionContext,
-          getHandler: () => new MockClass().test1 as any,
-        });
-        expect(guard.canActivate(executionContext)).toBe(false);
-      });
-
-      it('should return false when test2 requires permission from handler', () => {
-        SetMetadata('Permission', ['HandlerPermission'])(mockHttpRequest.app);
-        const executionContext = createMock<ExecutionContext>({
-          ...mockHttpExecutionContext,
-          getHandler: () => new MockClass().test2 as any,
-        });
-        expect(guard.canActivate(executionContext)).toBe(false);
-      });
-
-      it('should return false when test3 requires permission from class', () => {
-        const executionContext = createMock<ExecutionContext>({
-          ...mockHttpExecutionContext,
-          getHandler: () => new MockClass().test3 as any,
-        });
-        expect(guard.canActivate(executionContext)).toBe(false);
-      });
-
-      it('should return false when test4 requires role from class', () => {
-        const executionContext = createMock<ExecutionContext>({
-          ...mockHttpExecutionContext,
-          getHandler: () => new MockClass().test4 as any,
-        });
-        expect(guard.canActivate(executionContext)).toBe(false);
       });
     });
   });
